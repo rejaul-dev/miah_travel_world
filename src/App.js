b193e5efa1f1adec7fe993f602726703fe426ee1
rejaul-karim10 <rejalkarim66666@gmail.com>
2023-02-05 07:36:31 +0800

    adding topbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import TopBar from './components/TopBar';
 
 function App() {
   return (
-   <h1>Hello World</h1>
+   <div>
+    <TopBar/>
+   </div>
   );
 }
 
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { AiFillPhone, AiOutlineClockCircle } from "react-icons/ai"
+import { BsChatSquareDots } from "react-icons/bs"
+
+const TopBar = () => {
+  return (
+    <div className='flex justify-between items-center px-4 py-2'>
+        <div className='flex items-center'>
+            <BsChatSquareDots size={30} className="text-cyan-400 mr-2"/>
+            <h1 className='text-xl font-bold text-gray-700'>Miah Travel World</h1>
+        </div>
+        <div className='flex'>
+            <div className='hidden md:flex items-center px-6'>
+                <AiOutlineClockCircle size={20} className="mr-2 text-cyan-400"/>
+                <p className='text-sm text-gray-700'>9am to 5pm</p>
+            </div>
+            <div className='hidden md:flex items-center px-6'>
+                <AiFillPhone size={20} className="mr-2 text-cyan-400"/>
+                <p className='text-sm text-gray-700'>018384674746</p>
+            </div>
+            <button>Get a free Quote</button>
+        </div>
+    </div>
+  )
+}
+
+export default TopBar
